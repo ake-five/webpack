05fc6432d484b71560efb297663935213cd4cefd
ext.gaomengmeng6 <ext.gaomengmeng6@jd.com>
2024-01-08 11:49:53 +0800

    添加 return url 序列化

--- src/index.js
+++ src/index.js
@@ -61,7 +61,7 @@ function FunctionComponent() {
     if (!getCookie('ther')) {
       const envLoginDomainDev = '//ake-five.github.io'
       const returnURl = window.location.href
-      window.location.href = `${envLoginDomainDev}/login-vite-vue/?returnUrl=${returnURl}`//本地调试登陆地址
+      window.location.href = `${envLoginDomainDev}/login-vite-vue/?returnUrl=${encodeURIComponent(returnURl)}`//本地调试登陆地址
       return
     }
   }, [])
